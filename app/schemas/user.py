8d0faa80dac440eb329ed5957ca6d8d8dd2a98a7
Karlo Sebastian Cadena Cota <sebastian.cota86@gmail.com>
2026-05-12 14:22:37 -0600

    feat: "Set-Up"

---
+++ app/schemas/user.py
@@ -0,0 +1,21 @@
+import uuid
+from datetime import datetime
+
+from sqlmodel import SQLModel
+
+
+class UserCreate(SQLModel):
+    username: str
+    name: str
+    lastname: str
+    email: str
+    password: str
+
+
+class UserRead(SQLModel):
+    id: uuid.UUID
+    username: str
+    name: str
+    lastname: str
+    email: str
+    created_at: datetime
